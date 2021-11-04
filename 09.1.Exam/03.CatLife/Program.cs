ab2da13e3c3147e96ddab02eb13fd50e139fd966
vebili <74007431+vebili@users.noreply.github.com>
2021-11-04 13:07:45 +0200

    Up

---
+++ 09.1.Exam/03.CatLife/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string breed = Console.ReadLine();
+            char gender = char.Parse(Console.ReadLine());
+            double catYears = 0;
+            bool check = false;
+
+            switch (breed)
+            {
+                case "British Shorthair":
+                    if (gender == 'm')
+                    {
+                        catYears = 13;
+                    }
+                    else if (gender == 'f')
+                    {
+                        catYears = 14;
+                    }
+                    break;
+                case "Siamese":
+                    if (gender == 'm')
+                    {
+                        catYears = 15;
+                    }
+                    else if (gender == 'f')
+                    {
+                        catYears = 16;
+                    }
+                    break;
+                case "Persian":
+                    if (gender == 'm')
+                    {
+                        catYears = 14;
+                    }
+                    else if (gender == 'f')
+                    {
+                        catYears = 15;
+                    }
+                    break;
+                case "Ragdoll":
+                    if (gender == 'm')
+                    {
+                        catYears = 16;
+                    }
+                    else if (gender == 'f')
+                    {
+                        catYears = 17;
+                    }
+                    break;
+                case "American Shorthair":
+                    if (gender == 'm')
+                    {
+                        catYears = 12;
+                    }
+                    else if (gender == 'f')
+                    {
+                        catYears = 13;
+                    }
+                    break;
+                case "Siberian":
+                    if (gender == 'm')
+                    {
+                        catYears = 11;
+                    }
+                    else if (gender == 'f')
+                    {
+                        catYears = 12;
+                    }
+                    break;
+                default:
+                    Console.WriteLine($"{breed} is invalid cat!");
+                    check = true;
+                    break;
+            }
+
+            if (check == false)
+            {
+                double catMonths = (catYears * 12) / 6;
+                Console.WriteLine(catMonths + " cat months");
+            }
+
+
+        }
+    }
+}
---
+++ 09.1.Exam/04.CatFood/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.CatFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+                int numbOfCats = int.Parse(Console.ReadLine());
+
+                int catCounter = 1;
+                int group1 = 0;
+                int group2 = 0;
+                int group3 = 0;
+                double totalFoodWeightGrams = 0;
+                while (catCounter <= numbOfCats)
+                {
+                    double foodWeightInGrams = double.Parse(Console.ReadLine());
+                    totalFoodWeightGrams += foodWeightInGrams;
+                    if (foodWeightInGrams >= 100 && foodWeightInGrams < 200)
+                    {
+                        group1++;
+                    }
+                    else if (foodWeightInGrams >= 200 && foodWeightInGrams < 300)
+                    {
+                        group2++;
+                    }
+                    else if (foodWeightInGrams >= 300 && foodWeightInGrams <= 400)
+                    {
+                        group3++;
+                    }
+
+                    catCounter++;
+                }
+
+                double priceForFood = (totalFoodWeightGrams / 1000) * 12.45;
+            priceForFood = Math.Floor(priceForFood * 100) / 100;
+            Console.WriteLine($"Group 1: {group1} cats.");
+                Console.WriteLine($"Group 2: {group2} cats.");
+                Console.WriteLine($"Group 3: {group3} cats.");
+                Console.WriteLine($"Price for food per day: {priceForFood:F2} lv.");
+        }
+    }
+}
